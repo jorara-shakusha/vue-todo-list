12b648c7f9a2572a683e88145b4b9779125f9700
Fujita Kazuhiko <tutuhelvetica2@gmail.com>
2020-06-27 11:48:57 +0900

    STEP11 選択用フォームの作成

--- main.js
+++ main.js
@@ -20,7 +20,15 @@ const app = new Vue({
   el: "#app",
   data: {
     // 使用するデータ
-    todos: []
+    todos: [],
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0,  label: '作業中' },
+      { value: 1,  label: '完了' }
+    ],
+    // 選択している options の value を記憶するためのデータ
+    // 初期値を「-1」つまり「すべて」にする
+    current: -1
   },
   methods: {
     // 使用するメソッド
